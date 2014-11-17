16dcced6641b3123e4c9fc1eb98bf62f8279f10c
Thatipally <thatipallym1337@admins-iMac-6.local>
2014-11-17 11:38:33 -0600

    Initial Commit

---
+++ RecipeAppTests/RecipeAppTests.m
@@ -0,0 +1,34 @@
+//
+//  RecipeAppTests.m
+//  RecipeAppTests
+//
+//  Created by Thatipally, Monika on 11/17/14.
+//  Copyright (c) 2014 ___FULLUSERNAME___. All rights reserved.
+//
+
+#import <XCTest/XCTest.h>
+
+@interface RecipeAppTests : XCTestCase
+
+@end
+
+@implementation RecipeAppTests
+
+- (void)setUp
+{
+    [super setUp];
+    // Put setup code here. This method is called before the invocation of each test method in the class.
+}
+
+- (void)tearDown
+{
+    // Put teardown code here. This method is called after the invocation of each test method in the class.
+    [super tearDown];
+}
+
+- (void)testExample
+{
+    XCTFail(@"No implementation for \"%s\"", __PRETTY_FUNCTION__);
+}
+
+@end
